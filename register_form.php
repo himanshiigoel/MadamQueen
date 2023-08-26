a4cac59312ec79d464864821bc49106febeae4c6
Himanshi <98215684+himanshiigoel@users.noreply.github.com>
2023-08-26 23:04:59 +0530

    Add files via upload

---
+++ register_form.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Registration Form</title>
+    </head>
+    <body>
+        <h1>Registration</h1>
+        <form method="post" action="register_submit.php">
+            name: <input type="text" name="name"/><br/><br/>
+            email: <input type="text" name="email"/><br/><br/>
+            password: <input type="password" name="password"/><br/><br/>
+            <input type="submit" name="submit" value="submit">
+        </form>
+    </body>
+</html>
